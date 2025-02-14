81763d16d23bca9257fc837c8410b52efe74c466
Daniel McRae <Daniel.McRae@signature.com>
2025-02-14 18:53:17 -0500

    fix add_created_workload

--- script/add_created_workload.py
+++ script/add_created_workload.py
@@ -5,7 +5,7 @@ from datetime import date
 import pandas
 
 
-def append_line(name: str, purpose: str, created_by: str, created_date: date):
+def prepend_line(name: str, purpose: str, created_by: str, created_date: date):
     df = pandas.read_csv("temp_data/workloads_created.csv")
 
     pandas.concat(
@@ -31,16 +31,17 @@ def append_line(name: str, purpose: str, created_by: str, created_date: date):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         prog="add_created_worload.py",
-        description="Appends a line to the workloads_created file.",
+        description="Prepends a line to the workloads_created file.",
     )
     parser.add_argument("name")
     parser.add_argument("purpose")
     parser.add_argument("created_by")
-
-    append_line(
-        name=parser.name,
-        purpose=parser.purpose,
-        created_by=parser.created_by,
+    args = parser.parse_args()
+    
+    prepend_line(
+        name=args.name,
+        purpose=args.purpose,
+        created_by=args.created_by,
         created_date=date.today(),
     )
     
