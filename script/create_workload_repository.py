8726b8b33757afb21b2f7711967b6ea0a9f606fe
Daniel McRae <Daniel.McRae@signature.com>
2025-02-17 02:42:47 -0500

    going to python

--- script/create_workload_repository.py
+++ script/create_workload_repository.py
@@ -1,4 +1,5 @@
 from os import environ
+import time
 
 from .repository import github
 
@@ -8,10 +9,11 @@ def build_repository():
     name = f"rg-{environ["ENVIRONMENT"]}-{environ["ROLE"]}-{environ["COUNTER"].zfill(3)}"
     repository_definition = github.create_repository_from_template(auth, name, environ["DESCRIPTION"])
 
+    time.sleep(30)
     html_url = repository_definition["html_url"]
     full_name = repository_definition["full_name"]
     contents_url = repository_definition["contents_url"].replace("{+path}", "")
-    github.add_file_to_repository(auth, contents_url, "main.bicep", "initial commit")
+    github.add_file_to_repository(auth, contents_url, "main.bicep", "created config")
 
 
 if __name__ == "__main__":
