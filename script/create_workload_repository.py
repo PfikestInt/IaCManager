bc118a7ca5719fcb75dc284e8c456f184ccf94c2
Daniel McRae <Daniel.McRae@signature.com>
2025-02-17 02:45:56 -0500

    going to python

--- script/create_workload_repository.py
+++ script/create_workload_repository.py
@@ -9,7 +9,7 @@ def build_repository():
     name = f"rg-{environ["ENVIRONMENT"]}-{environ["ROLE"]}-{environ["COUNTER"].zfill(3)}"
     repository_definition = github.create_repository_from_template(auth, name, environ["DESCRIPTION"])
 
-    time.sleep(30)
+    time.sleep(5)
     html_url = repository_definition["html_url"]
     full_name = repository_definition["full_name"]
     contents_url = repository_definition["contents_url"].replace("{+path}", "")
