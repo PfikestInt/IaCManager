922333784520fbb10f9e8e7f7efeb4841d5ad7dc
Daniel McRae <Daniel.McRae@signature.com>
2025-02-16 23:10:27 -0500

    going to python

--- script/create_workload_repository.py
+++ script/create_workload_repository.py
@@ -1,16 +1,43 @@
+from os import environ
 
-import json
-from os import environ, path
-import sys
-sys.path.append(path.dirname(__file__))
+import requests
 
-from .repository import github
+
+class GitHubRepository:
+    @classmethod
+    def create_repository_from_template(authorization, name, description):
+        headers = {
+            "Accept": "application/vnd.github+json",
+            "Authorization": f"Bearer {authorization}",
+            "X-GitHub-Api-Version": "2022-11-28",
+        }
+        data = [
+            '"owner":"PfikestInt"',
+            f'"name":"{name}"',
+            f'"description":"{description}"',
+            '"include_all_branches":true',
+            '"private":false'
+        ]
+
+        response = requests.post(
+            url="https://api.github.com/repos/PfikestInt/WorkloadTemplate/generate",
+            headers=headers,
+            data="{" + ",".join(data) + "}",
+        ).json()
+
+        if response.get("status", "200") != "200":
+            for error in response["errors"]:
+                print(error)
+            exit(1)
+        
+        return response
+
 
 def build_repository():
     auth = environ["AUTH_TOKEN"]
 
     name = f"rg-{environ["ENVIRONMENT"]}-{environ["ROLE"]}-{environ["COUNTER"].zfill(3)}"
-    repository_definition = github.create_repository_from_template(auth, name, environ["DESCRIPTION"])
+    repository_definition = GitHubRepository.create_repository_from_template(auth, name, environ["DESCRIPTION"])
 
     print(repository_definition)
 
