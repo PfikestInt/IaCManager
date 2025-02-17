9a854b243e6e2f168a37d853b17874d807c0e72f
Daniel McRae <Daniel.McRae@signature.com>
2025-02-16 21:57:25 -0500

    going to python

--- script/create_workload_repository.py
+++ script/create_workload_repository.py
@@ -1,17 +1,40 @@
 
 from os import environ
 
-from github import Auth, Github
+import requests
 
 def build_repository():
-    auth = Auth.Token(environ["AUTH_TOKEN"])
-    gh = Github(auth=auth)
+    """
+        curl -L \
+          -X POST \
+          -H "Accept: application/vnd.github+json" \
+          -H "Authorization: Bearer ${{ steps.app_token.outputs.token }}" \
+          -H "X-GitHub-Api-Version: 2022-11-28" \
+          https://api.github.com/repos/PfikestInt/WorkloadTemplate/generate \
+          -d '{"owner":"PfikestInt","name":"rg-${{ github.event.inputs.environment-type }}-${{ github.event.inputs.role }}-${{ env.PADDED_COUNTER }}","description":"${{ github.event.inputs.description }}","include_all_branches":true,"private":false}' > results.json
+        echo "REPO_URL=$(jq '.html_url' results.json)" >> "$GITHUB_OUTPUT"
+    """
+    auth = environ["AUTH_TOKEN"]
 
-    for repo in gh.get_user().get_repos():
-        print(repo.name)
+    headers = {
+        "Accept": "application/vnd.github+json",
+        "Authorization": f"Bearer {auth}",
+        "X-GitHub-Api-Version": "2022-11-28",
+    }
+    data = {
+            "owner":"PfikestInt",
+            "name": f"rg-{environ["ENVIRONMENT"]}-{environ["ROLE"]}-{environ["COUNTER"].zfill(3)}",
+            "description": environ["DESCRIPTION"],
+            "include_all_branches": "true",
+            "private": "false"
+        }
+    response = requests.post(
+        url="https://api.github.com/repos/PfikestInt/WorkloadTemplate/generate",
+        headers=headers,
+        data=data,
+    )
+    print(response.text)
 
-    gh.close()
-
-
+    
 if __name__ == "__main__":
     build_repository()
