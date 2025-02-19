0ed81da05123e89b69ece300b9216ebd9f98105c
Daniel McRae <Daniel.McRae@signature.com>
2025-02-19 08:40:23 -0500

    updated deploy

--- main.tf
+++ main.tf
@@ -1,22 +1,6 @@
 data "azurerm_subscription" "current" {
 }
 
-resource "azurerm_user_assigned_identity" "managed_identity" {
-  location            = var.location
-  name                = var.location == "eastus" ? "id-${var.environment}-github-001" : "id-${var.environment}-github-${var.location}-001"
-  resource_group_name = var.location == "eastus" ? "rg-${var.environment}-${var.role}-${var.counter}" : "rg-${var.environment}-${var.role}-${var.location}-${var.counter}"
-  tags                = local.tags
-}
-
-resource "azurerm_federated_identity_credential" "credentials" {
-  name                = "github-main"
-  resource_group_name = module.resource_group.name
-  audience            = ["api://AzureADTokenExchange"]
-  issuer              = "https://token.actions.githubusercontent.com"
-  parent_id           = azurerm_user_assigned_identity.managed_identity.id
-  subject             = "repo:${var.repository_name}:ref:refs/heads/main"
-}
-
 module "resource_group" {
   source           = "Azure/avm-res-resources-resourcegroup/azurerm"
   version          = "0.2.1"
@@ -60,4 +44,24 @@ resource "azurerm_resource_group_policy_assignment" "azure_cis_policy" {
     }
   }
   EOT
+}
+
+resource "azurerm_user_assigned_identity" "managed_identity" {
+  location            = var.location
+  name                = var.location == "eastus" ? "id-${var.environment}-github-001" : "id-${var.environment}-github-${var.location}-001"
+  resource_group_name = var.location == "eastus" ? "rg-${var.environment}-${var.role}-${var.counter}" : "rg-${var.environment}-${var.role}-${var.location}-${var.counter}"
+  tags                = local.tags
+
+  depends_on = [
+    module.resource_group
+  ]
+}
+
+resource "azurerm_federated_identity_credential" "credentials" {
+  name                = "github-main"
+  resource_group_name = module.resource_group.name
+  audience            = ["api://AzureADTokenExchange"]
+  issuer              = "https://token.actions.githubusercontent.com"
+  parent_id           = azurerm_user_assigned_identity.managed_identity.id
+  subject             = "repo:${var.repository_name}:ref:refs/heads/main"
 }
